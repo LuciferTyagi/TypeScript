f57f270851ff125a5e76215c9caada935789f815
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-07-04 12:49:55 +0530

    Type Inference

--- 01/variable.ts
+++ 01/variable.ts
@@ -1,6 +1,12 @@
 let greetings: string = "Hello Yagyansh";
 
-let mynum: Number = 6;
+let mynum = 6;
+//It`s not neccessary to always use : and defined type
+//TypeScript is smart enough and this is called Type inference
 console.log(greetings);
+
+let userId: number = 12345.343;
+
+let isLoggedIn: boolean = false;
 
 export {};
