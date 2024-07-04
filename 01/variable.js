dff9464b5a0e7b3aa51b4e9706c6fefad72d388f
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-07-04 12:21:47 +0530

    some variables

---
+++ 01/variable.js
@@ -0,0 +1,2 @@
+var greetings = "Hello Yagyansh";
+console.log(greetings);
---
+++ 01/variable.ts
@@ -0,0 +1,6 @@
+let greetings: string = "Hello Yagyansh";
+
+let mynum: Number = 6;
+console.log(greetings);
+
+export {};
